1f92f12593e0f3e4234d0bc4271b310ca972ab49
Vicaris <vicarisfsc@gmail.com>
2022-08-23 01:17:34 +0200

    Add search bar and recipe count

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
-from ast import Del, Delete
 from audioop import reverse
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 from .models import Recipe
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 # Create your views here.
 
@@ -12,6 +12,16 @@ class RecipeList(ListView):
     model = Recipe
     template_name = "app/recipe_list.html"
     context_object_name = "recipes"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['count'] = context['recipes'].count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['recipes'] = context['recipes'].filter(Q(name__icontains=search_input) | Q(requirements__icontains=search_input))
+            context['search_input'] = search_input
+        return context
 
 
 class RecipeDetail(DetailView):
