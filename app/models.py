be27eecd05b40eff9f84ed5118cd8d9a661095d0
Vicaris <vicarisfsc@gmail.com>
2022-08-22 18:58:20 +0200

    Add Recipe and Category model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=80)
+
+
+class Recipe(models.Model):
+    name = models.CharField(blank=False, max_length=80)
+    description = models.TextField(blank=True)
+    requirements = models.TextField(blank=True)
+    instruction =  models.TextField(blank=True)
+    prep_time = models.IntegerField(blank=True)
+    price = models.IntegerField(blank=True)
+    publish_date = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, blank=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        # More Meta options:
+        # https://docs.djangoproject.com/en/4.1/ref/models/options/
+        ordering = ['-prep_time']
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView, DetailView, DeleteView, CreateView
 
 # Create your views here.
+
+
+class RecipeList(ListView):
+    pass
