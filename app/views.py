cda41f09dcbb8715f59be602b79b32933e8ee150
Vicaris <vicarisfsc@gmail.com>
2022-08-24 00:30:49 +0200

    Delete unnecessary views

--- app/views.py
+++ app/views.py
@@ -28,48 +28,10 @@ class RecipeList(ListView):
             context["recipes"] = context['recipes'].filter(category__icontains=choose_category)
             context['chosen_category'] = choose_category
         if search_input:
-            context['recipes'] = context['recipes'].filter(
-                Q(name__icontains=search_input) | Q(requirements__icontains=search_input))[:2]
+            context['recipes'] = context['recipes'].filter(Q(name__icontains=search_input) | Q(requirements__icontains=search_input) | Q(description__icontains=search_input))
                 # https://docs.djangoproject.com/en/4.1/topics/db/queries/#lookups-that-span-relationships
             context['search_input'] = search_input
         return context
-
-
-
-
-
-class LunchRecipeList(ListView):
-    model = Recipe
-    context_object_name = "recipes"
-    template_name = "app/recipe_list.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['recipes'] = context['recipes'].filter(category__icontains="Lunch")
-        return context
-
-
-class DinnerRecipeList(ListView):
-    model = Recipe
-    context_object_name = "recipes"
-    template_name = "app/recipe_list.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['recipes'] = context['recipes'].filter(category__icontains="Dinner")
-        return context
-
-
-class BreakfastRecipeList(ListView):
-    model = Recipe
-    context_object_name = "recipes"
-    template_name = "app/recipe_list.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['recipes'] = context['recipes'].filter(category__icontains="Breakfast")
-        return context
-
 
 
 class RecipeDetail(DetailView):
