f2b12ff4b6d8146dc36bfd9df60a54de8ac88e31
Vicaris <vicarisfsc@gmail.com>
2022-10-01 09:55:13 +0200

    Clean code

--- project/urls.py
+++ project/urls.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include("app.urls")),
-    path('admin/', admin.site.urls),
+    # automatically create url for uploaded image
+    path('admin/', admin.site.urls)] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # automatically create url for uploaded image
-# path('/', include('app.urls'))
